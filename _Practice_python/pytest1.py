ecdb384f2159cdc4a1f85cf1df4b5f1306db8d1f
secretscav <107612318+secretb4r@users.noreply.github.com>
2022-07-12 15:35:09 +0000

    new file added

---
+++ _Practice_python/pytest1.py
@@ -0,0 +1,10 @@
+import itertools
+import inspect
+from time import sleep
+
+class Man:
+    def __init__(self, name, age, nationality) -> object:
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+
